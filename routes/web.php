1bd84e9ec82a5b6acff3eb9f55daba1196f26aaa
dhaydt <1992dayat@gmail.com>
2022-08-27 18:31:15 +0700

    storage-link

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FlipController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+});
 
 Route::get('/', [FlipController::class, 'index'])->name('home');
 
